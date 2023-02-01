4baaae59c98d1cf785001f864bb1ea7e8891ef17
justeyallevi@gmail.com <justeyallevi@gmail.com>
2023-02-01 10:06:33 +0200

    add the Vision subsystems

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Vision;
 import frc.util.dashboardUtil.TimerWidget;
 import frc.util.dashboardUtil.TimerWidget.Mode;
 
@@ -140,6 +141,7 @@ public class Robot extends LoggedRobot {
     if (autonomousCommand != null) {
       autonomousCommand.cancel();
     }
+    Vision.GetInstance();
   }
 
   /** This function is called periodically during operator control. */
---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.io.IOException;
+import java.lang.annotation.Target;
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.networktables.NetworkTableType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+  private static Vision instance;
+  /** Creates a new PhotonVision. */
+  public PhotonCamera camera = new PhotonCamera("mariners-cam");
+  public Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
+
+  public static Vision GetInstance(){
+    if(instance == null){
+      instance = new Vision();
+    }
+    return instance;
+  }
+  AprilTagFieldLayout aprilTagFieldLayout;
+  
+  PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.LOWEST_AMBIGUITY, camera, robotToCam);
+  
+   
+  private Vision() {
+    
+    try {
+      aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+    
+  }
+
+ 
+  
+
+  @Override
+  public void periodic() {
+    var result = camera.getLatestResult();
+    result = camera.getLatestResult();
+    if(result.hasTargets()){
+      PhotonTrackedTarget target = result.getBestTarget();
+      int TargetID = target.getFiducialId();
+      double Pose = target.getPoseAmbiguity();
+      Transform3d trans = target.getBestCameraToTarget();
+      SmartDashboard.putBoolean("Has target", result.hasTargets());
+      SmartDashboard.putNumber("ID", TargetID);
+      SmartDashboard.putNumber("ambgouitu", Pose);
+    }
+    else{
+      SmartDashboard.putBoolean("Has target", result.hasTargets());
+      SmartDashboard.putNumber("ID", 0);
+      SmartDashboard.putNumber("ambgouitu", 0);
+    }
+  }
+}
