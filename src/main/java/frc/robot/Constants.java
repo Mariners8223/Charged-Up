0a21774a6be184b18fe49b424009cbe448728917
captainsoccer <justeyallevi@gmail.com>
2023-02-14 15:12:37 +0200

    made the code prettier and added the gettimestamp function

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,19 +23,12 @@ public final class Constants {
   public static final Mode currentMode = Mode.REAL;
   public static final double fullThres = 0.99;
   public static final double heldThres = 0.1;
+  public static final Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5),new Rotation3d(  0, 0,  0));
+  public static final Transform3d robotToLimeLight = new Transform3d(new Translation3d(0.5, 0.0, 0.5),new Rotation3d(  0, 0,  0));
+  public static final boolean IsLimeLightAprilTags = false;
   static class FieldConstants {
     static final double length = Units.feetToMeters(54);
     static final double width = Units.feetToMeters(27);
-  }
-  static class VisionConstants {
-    static final Transform3d robotToCam =
-            new Transform3d(
-                    new Translation3d(0.5, 0.0, 0.5),
-                    new Rotation3d(
-                            0, 0,
-                            0)); // Cam mounted facing forward, half a meter forward of center, half a meter up
-    // from center.
-    static final String cameraName = "YOUR CAMERA NAME";
   }
   public enum Mode {
     /** Running on a real robot. */
