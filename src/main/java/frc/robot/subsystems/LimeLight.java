4ad3d35afe64a823703ed2d1942e056d8fc6c052
captainsoccer <justeyallevi@gmail.com>
2023-02-16 08:12:42 +0200

    made changes that all the april tags calcs are made in the Vision.java

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -25,15 +25,6 @@ import frc.robot.Constants;
 public class LimeLight extends SubsystemBase {
   //creates all the vars
   private static LimeLight Instance;
-  private static AprilTagFieldLayout aprilTagFieldLayout;
-  private final Field2d m_field = new Field2d();
-  private static Vision instance;
-  private static Pose3d pose3d;
-  private static Pose2d pose2d;    
-  private static Pose2d lastPose2d;
-  private static Pose3d lastPose3d;
-  private static PhotonCamera camera;
-  private static PhotonPoseEstimator photonPoseEstimator;
   
   //creates new instance
   public static LimeLight getInstance(){
@@ -43,44 +34,9 @@ public class LimeLight extends SubsystemBase {
 
   //creates the constructor
   private LimeLight() {
-    pose2d = new Pose2d(new Translation2d(0, 0), new Rotation2d(0));
-    lastPose2d = pose2d;
-    pose3d = new Pose3d(pose2d);
-    lastPose3d = pose3d;
-    camera = new PhotonCamera("limelight-mariners");
-    try {
-      aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
-    } catch (IOException e) {}
-    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, Constants.robotToLimeLight);
-  }
-
-  public Pose2d getPose2d(){
-    if(camera.getLatestResult().hasTargets()){
-      return pose2d;
-    }
-    else{
-      return null;
-    }
   }
 
 
-  public Pose3d getPose3d(){
-    if(camera.getLatestResult().hasTargets()){
-      return pose3d;
-    }
-    else{
-      return null;
-    }
-  }
-
-  public double getTimeStamp(){
-    if(camera.getLatestResult().hasTargets()){
-      return camera.getLatestResult().getTimestampSeconds();
-    }
-    else{
-      return 0;
-    }
-  }
 
   //preiodic shit
   @Override
