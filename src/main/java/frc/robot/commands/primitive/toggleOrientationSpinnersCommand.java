9678f85dc836a84c0f71aa6d51a36d371f5570ad
captainsoccer <justeyallevi@gmail.com>
2023-03-09 15:29:34 +0200

    im fixing stupid shit - roy

--- src/main/java/frc/robot/commands/primitive/toggleOrientationSpinnersCommand.java
+++ src/main/java/frc/robot/commands/primitive/toggleOrientationSpinnersCommand.java
@@ -1,8 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot.Commands.primitive;
+package frc.robot.commands.primitive;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.orientation.Orientation;
