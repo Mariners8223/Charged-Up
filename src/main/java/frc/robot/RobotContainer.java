68f78a3fc6c8d3af314602e1da3a1fb5483f7864
captainsoccer <justeyallevi@gmail.com>
2023-03-13 06:50:13 +0200

    changedkeybinds for alon

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -193,12 +193,12 @@ public class RobotContainer {
 
 
     driveController.cross().onTrue(new InstantCommand(() -> Drivebase.getInstance().resetGyro()));
-    driveController.square().whileTrue(new InstantCommand(() -> SwerveModuleConstants.freeSpeedMetersPerSecond = 2.0)).onFalse(new InstantCommand(() -> SwerveModuleConstants.freeSpeedMetersPerSecond = 4.0));
+    driveController.R1().onTrue(new InstantCommand(() -> Constants.Drivetrain.SwerveModuleConstants.freeSpeedMetersPerSecond = 2)).onFalse(new InstantCommand(() -> Constants.Drivetrain.SwerveModuleConstants.freeSpeedMetersPerSecond = 4 ));
     driveController.L1().whileTrue(new intakeCommand(0.6));
     driveController.L2().onTrue(new InstantCommand(() -> Orientation.getInstance().lowerOrientation())); driveController.L2().onFalse(new InstantCommand(() -> Orientation.getInstance().raiseOrientation()));
     driveController.L2().onTrue(new InstantCommand(() -> Orientation.getInstance().lowerRamp())); driveController.L2().onFalse(new InstantCommand(() -> Orientation.getInstance().raiseRamp()));
-    driveController.R1().onTrue(new InstantCommand(() -> Orientation.getInstance().setSpeed(0.6))); driveController.R1().onFalse(new InstantCommand(() -> Orientation.getInstance().stop()));
-    driveController.R2().onTrue(new InstantCommand(() -> Orientation.getInstance().setSpeed(-0.6))); driveController.R2().onFalse(new InstantCommand(() -> Orientation.getInstance().stop()));
+    driveController.povUp().onTrue(new InstantCommand(() -> Orientation.getInstance().setSpeed(0.6))); driveController.R1().onFalse(new InstantCommand(() -> Orientation.getInstance().stop()));
+    driveController.povDown().onTrue(new InstantCommand(() -> Orientation.getInstance().setSpeed(-0.6))); driveController.R2().onFalse(new InstantCommand(() -> Orientation.getInstance().stop()));
     
 
 
