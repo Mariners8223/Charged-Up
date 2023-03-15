a09f5b624d5b950206b1144101326db63341e2fc
captainsoccer <justeyallevi@gmail.com>
2023-03-15 13:28:14 +0200

    commented out old drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.Drivetrain;
 import frc.robot.Constants.Drivetrain.SwerveModuleConstants;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.MoveArmToSetPoint;
 import frc.robot.commands.Autonomous.Autos;
 import frc.robot.commands.Autonomous.BalanceOnRamp;
 import frc.robot.commands.Autonomous.EnterRamp;
@@ -112,10 +111,10 @@ public class RobotContainer {
     autoChooser.addOption("Balance", new SequentialCommandGroup(new StartEnterRamp(), new EnterRamp(), new BalanceOnRamp()));
     // Configure the button bindings
 
-    Drivebase.getInstance().setDefaultCommand(new RunCommand(() -> {
-      Drivebase.getInstance().drive(-RobotContainer.calculateDeadband(getDriveControllerRawAxis(0)) * SwerveModuleConstants.freeSpeedMetersPerSecond,
-          RobotContainer.calculateDeadband(getDriveControllerRawAxis(1)) * SwerveModuleConstants.freeSpeedMetersPerSecond, RobotContainer.calculateDeadband(getDriveControllerRawAxis(2)) * -12.5);
-    }, Drivebase.getInstance()));
+    // Drivebase.getInstance().setDefaultCommand(new RunCommand(() -> {
+    //   Drivebase.getInstance().drive(-RobotContainer.calculateDeadband(getDriveControllerRawAxis(0)) * SwerveModuleConstants.freeSpeedMetersPerSecond,
+    //       RobotContainer.calculateDeadband(getDriveControllerRawAxis(1)) * SwerveModuleConstants.freeSpeedMetersPerSecond, RobotContainer.calculateDeadband(getDriveControllerRawAxis(2)) * -12.5);
+    // }, Drivebase.getInstance()));
 
     configureButtonBindings();
 
